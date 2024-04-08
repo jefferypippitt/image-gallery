c190f1653455d272d6cfdcdc472fd5cf740b45ec
Jeffery <jefferyspippitt@gmail.com>
2024-04-08 00:33:11 -0500

    added supabase structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { fetchImages } from '@/app/index'
+import { fetchImages } from '@/app/utils/supabase/server'
 import Link from 'next/link'
 import {
   Card,
@@ -10,10 +10,6 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
-
-function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
 
 type ImageData = {
   id: number
@@ -41,7 +37,7 @@ export default function Page() {
 
   return (
     <div className='container'>
-      <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-center'>
+      <h2 className='scroll-m-20 pb-2 text-center text-3xl font-semibold tracking-tight first:mt-0'>
         Image Gallery Project
       </h2>
       <div className='grid grid-cols-1 gap-6 p-4 md:grid-cols-2 md:p-6 lg:grid-cols-3 xl:grid-cols-4'>
@@ -51,6 +47,7 @@ export default function Page() {
             className='group'
             key={image.id}
             target='_blank'
+            
           >
             <Card>
               <div className='aspect-[4/3] overflow-hidden'>
